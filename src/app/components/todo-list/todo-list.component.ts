08c70d963a49d0560138fd2167afcf7cc35a7e82
thiagopls1 <thiagopls1@hotmail.com>
2023-09-03 21:42:51 -0300

    adicionando interface das tarefas

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -7,6 +7,7 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { Task } from 'src/app/task';
 
 @Component({
   selector: 'app-todo-list',
@@ -16,15 +17,63 @@ import {
   imports: [CdkDrag, CdkDropList, NgFor]
 })
 export class TodoListComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  inprogress = ['Answer messages'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  
+  Tasks: Task[] = [
+    {
+      title: "Get to work",
+      status: "Todo"
+    },
+    {
+      title: "Pick up groceries",
+      status: "Todo"
+    },
+    {
+      title: "Go home",
+      status: "Todo"
+    },
+    {
+      title: "Fall asleep",
+      status: "Todo"
+    },
+    {
+      title: "Answer messages",
+      status: "InProgress"
+    },
+    {
+      title: "Get up",
+      status: "Done"
+    },
+    {
+      title: "Bursh teeth",
+      status: "Done"
+    },
+    {
+      title: "Take a shower",
+      status: "Done"
+    },
+    {
+      title: "Check e-mail",
+      status: "Done"
+    },
+    {
+      title: "Walk dog",
+      status: "Done"
+    },
+  ]
 
-  drop(event: CdkDragDrop<string[]>){
+  todo = this.Tasks.filter(t => t.status == "Todo");
+  inprogress = this.Tasks.filter(t => t.status == "InProgress");
+  done = this.Tasks.filter(t => t.status == "Done");
+  
+  drop(event: CdkDragDrop<Task[]>){
     if(event.previousContainer === event.container){
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+      event.item.data.status = event.container.element.nativeElement.id
+      console.log(event.item.data)
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+      event.item.data.status = event.container.element.nativeElement.id
+      console.log(event.item.data)
     }
   }
 }
