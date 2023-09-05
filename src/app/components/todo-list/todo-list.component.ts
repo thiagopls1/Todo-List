b75f460cbcf1a3101c05563db3cf3afc582aebc3
thiagopls1 <thiagopls1@hotmail.com>
2023-09-05 10:51:57 -0300

    added mocks

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -7,9 +7,10 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
-import { Task } from 'src/app/task';
+import { Task } from 'src/app/interfaces/task';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Tasks } from 'src/app/mocks/mock-tasks';
 
 @Component({
   selector: 'app-todo-list',
@@ -20,58 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class TodoListComponent {
   
-  Tasks: Task[] = [
-    {
-      title: "Get to work",
-      description: "The book is on the table",
-      status: "Todo"
-    },
-    {
-      title: "Pick up groceries",
-      description: "The book is on the table",
-      status: "Todo"
-    },
-    {
-      title: "Go home",
-      description: "The book is on the table",
-      status: "Todo"
-    },
-    {
-      title: "Fall asleep",
-      description: "The book is on the table",
-      status: "Todo"
-    },
-    {
-      title: "Answer messages",
-      description: "The book is on the table",
-      status: "InProgress"
-    },
-    {
-      title: "Get up",
-      description: "The book is on the table",
-      status: "Done"
-    },
-    {
-      title: "Bursh teeth",
-      description: "The book is on the table",
-      status: "Done"
-    },
-    {
-      title: "Take a shower",
-      description: "The book is on the table",
-      status: "Done"
-    },
-    {
-      title: "Check e-mail",
-      description: "The book is on the table",
-      status: "Done"
-    },
-    {
-      title: "Walk dog",
-      description: "The book is on the table",
-      status: "Done"
-    },
-  ]
+  Tasks: Task[] = Tasks;
 
   todo = this.Tasks.filter(t => t.status == "Todo");
   inprogress = this.Tasks.filter(t => t.status == "InProgress");
---
+++ src/app/mocks/mock-tasks.ts
@@ -0,0 +1,54 @@
+import { Task } from "../interfaces/task";
+
+export const Tasks: Task[] = [
+  {
+    title: "Get to work",
+    description: "The book is on the table",
+    status: "Todo"
+  },
+  {
+    title: "Pick up groceries",
+    description: "The book is on the table",
+    status: "Todo"
+  },
+  {
+    title: "Go home",
+    description: "The book is on the table",
+    status: "Todo"
+  },
+  {
+    title: "Fall asleep",
+    description: "The book is on the table",
+    status: "Todo"
+  },
+  {
+    title: "Answer messages",
+    description: "The book is on the table",
+    status: "InProgress"
+  },
+  {
+    title: "Get up",
+    description: "The book is on the table",
+    status: "Done"
+  },
+  {
+    title: "Bursh teeth",
+    description: "The book is on the table",
+    status: "Done"
+  },
+  {
+    title: "Take a shower",
+    description: "The book is on the table",
+    status: "Done"
+  },
+  {
+    title: "Check e-mail",
+    description: "The book is on the table",
+    status: "Done"
+  },
+  {
+    title: "Walk dog",
+    description: "The book is on the table",
+    status: "Done"
+  },
+]
