c0e3a9f56dfcbd83e7b0e0c78d3a25e0af03bd7c
thiagopls1 <thiagopls1@hotmail.com>
2023-11-13 16:34:31 -0300

    finished add new task feature

--- src/app/components/dialogs/dialog-new-task/dialog-new-task.component.ts
+++ src/app/components/dialogs/dialog-new-task/dialog-new-task.component.ts
@@ -1,26 +1,37 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { Task } from 'src/app/interfaces/task';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dialog-new-task-dialog',
   templateUrl: './dialog-new-task.component.html',
   styleUrls: ['./dialog-new-task.component.css'],
   standalone: true,
-  imports: [MatSelectModule, MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule]
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    NgFor
+  ]
 })
 export class DialogNewTaskComponent {
   statuses: string[] = [
-    "ToDo",
+    "Todo",
     "InProgress",
     "Done"
   ];
-  
+
   constructor(
     public dialogRef: MatDialogRef<DialogNewTaskComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Task,
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,56 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-  CdkDrag,
-  CdkDropList,
-} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { Task } from 'src/app/interfaces/task';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Tasks } from 'src/app/mocks/mock-tasks';
 import { TaskComponent } from '../task/task.component';
+import { DialogNewTaskComponent } from '../dialogs/dialog-new-task/dialog-new-task.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
-  imports: [CdkDrag, CdkDropList, NgFor, MatButtonModule, MatIconModule, TaskComponent, CommonModule, MatDialogModule]
+  imports: [
+    DialogNewTaskComponent,
+    CdkDrag,
+    CdkDropList,
+    NgFor,
+    MatButtonModule,
+    MatIconModule,
+    TaskComponent,
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ]
 })
 
-// Dialog
-// export class TaskDialog
-// {
-//   task!: Task; 
-
-//   openDialog(): void {
-//     const dialogRef = this.dialog.open(TaskDialog, {
-//       data: this.task
-//     });
-
-//     this.dialogRef.afterClosed().
-//   }
-
-//   constructor(public dialog: MatDialog){ }
-// }
-
-// Page
 export class TodoListComponent {
-
+  constructor(public dialog: MatDialog, private ref:ChangeDetectorRef) { }
   Tasks: Task[] = Tasks;
 
-  todo = this.Tasks.filter(t => t.status == "Todo");
-  inprogress = this.Tasks.filter(t => t.status == "InProgress");
-  done = this.Tasks.filter(t => t.status == "Done");
+  ngOnInit() {
+    this.ref.detectChanges();
+  }
 
   checkStatus(task: Task, status: string): boolean {
     return (task.status == status);
   }
-
 
   drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) { // Same container
@@ -58,8 +56,8 @@ export class TodoListComponent {
       event.item.data.status = event.container.element.nativeElement.id;
       return;
     }
-    
-    if (event.currentIndex > event.previousIndex) { 
+
+    if (event.currentIndex > event.previousIndex) {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex - 1);
       event.item.data.status = event.container.element.nativeElement.id;
     }
@@ -67,5 +65,16 @@ export class TodoListComponent {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
       event.item.data.status = event.container.element.nativeElement.id;
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogNewTaskComponent,
+      { data: { title: '', description: '', status: '' } })
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.Tasks.unshift(result);
+      console.log(Tasks);
+    })
   }
 }
