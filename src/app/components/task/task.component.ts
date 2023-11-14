fa3d1ac34f2f7e32095424a3b051af62f2a3b66f
thiagopls1 <thiagopls1@hotmail.com>
2023-11-14 09:31:49 -0300

    changed input to textarea

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -6,13 +6,14 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { Task } from 'src/app/interfaces/task';
 import { DialogEditTaskComponent } from '../dialogs/dialog-edit-task/dialog-edit-task.component';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css'],
-  imports: [CdkDrag, CdkDropList, MatButtonModule, MatIconModule, NgFor, MatDialogModule]
+  imports: [CdkDrag, CdkDropList, MatButtonModule, MatIconModule, NgFor, MatDialogModule, MatInputModule]
 })
 
 export class TaskComponent {
@@ -28,6 +29,7 @@ export class TaskComponent {
       { data: { title: this.task.title, description: this.task.description, status: this.task.status } })
 
     dialogRef.afterClosed().subscribe(result => {
+      if(!result) return;
       const index = this.tasks.indexOf(this.task);
       this.tasks.splice(index, 1, result);
       this.tasksChange.emit(this.tasks);
